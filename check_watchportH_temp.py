397b20e0973392a2065202d13a34b47308cf7bb1
Nathan <nsafran1217@gmail.com>
2019-09-05 21:00:03 -0400

    Change from FileNotFoundError to IOError for python2 compatability

--- check_watchportH_temp.py
+++ check_watchportH_temp.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
+#	check_watchportT.py
+# 	Copyright 2019 Nathan Safran
+#
+#	This script gets the temperature from a Digi Watchport Temperature Sensor.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	
 import sys
 import optparse
 import time
 import re
 import serial
 from serial import Serial
-
-
-
 
 
 # Display a list of options to be sent to the plugin
@@ -49,7 +64,7 @@ def main (options):
         serData = ''
         serData += serialPort.read(1)
         serialPort.close()
-    except FileNotFoundError:
+    except IOError:
         print ("ERROR: Unable to read sensor")
         sys.exit(3)
     #if it returns nothing exit with unknown
